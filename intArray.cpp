d4484012672086e9e2ee06839d7d4058dc37c2ea
ask0later <139331444+ask0later@users.noreply.github.com>
2023-09-03 01:18:04 +0300

    Update intArray.cpp

--- intArray.cpp
+++ intArray.cpp
@@ -25,12 +25,12 @@ int main()
     const matrix2.size_y = 2;
     const matrix2.size_x = 3;*/
 
-    int data1[3][2] = {{2, -1},
-                       {2,  1},
-                       {1,  5}};
+    int data1[matrix1.size_y][matrix1.size_x] = {{2,  3},
+                                                 {3, -1},
+                                                 {-3, 2}};
 
-    int data2[2][3] = {{3, -2,  1},
-                       {1, -3,  1}};
+    int data2[matrix2.size_y][matrix2.size_x] = {{2, -3, -1},
+                                                 {3,  1,  1}};
 
     matrix1.data = *data1;
     matrix2.data = *data2;
@@ -49,7 +49,7 @@ void PrintData1(MATRIX matrix1)
         printf("\n");
         for (size_t j = 0; j < matrix1.size_x; j++)
         {
-            printf("a[%d, %d] = ", i + 1, j + 1);
+            printf("a[%d, %d] = ", i, j);
                                     // size_x
             printf("%2d  ", *(matrix1.data + matrix1.size_x * i + j));
         }
@@ -64,7 +64,7 @@ void PrintData2(MATRIX matrix2)
         printf("\n");
         for (size_t j = 0; j < matrix2.size_x; j++)
         {
-            printf("b[%d, %d] = ", i + 1, j + 1);
+            printf("b[%d, %d] = ", i, j);
                                     // size_x
             printf("%2d  ", *(matrix2.data + matrix2.size_x * i + j));
         }
@@ -94,21 +94,9 @@ int SelectionProduct(MATRIX matrix1, MATRIX matrix2)
 
 void ResultProduct(MATRIX matrix1, MATRIX matrix2)
 {
-    //return 2
-
-    int data1[3][2] = {{2, -1},
-                       {2,  1},
-                       {1,  5}};
-
-    int data2[2][3] = {{3, -2,  1},
-                       {1, -3,  1}};
-
-
-
 
     MATRIX matrix3 = {.data = 0, .size_y = matrix1.size_y, .size_x = matrix2.size_x};
-    int data3[3][3] = {0};
-    matrix3.data = *data3;
+    int data3[matrix3.size_y][matrix3.size_x] = {0};
     size_t n = matrix1.size_x;
 
     for (size_t i = 0; i < matrix1.size_y; i++)
@@ -119,12 +107,15 @@ void ResultProduct(MATRIX matrix1, MATRIX matrix2)
             int val = 0;
             for (size_t k = 0; k < n; k++)
             {
-                val = val + data1[i][k] + data2[k][j];
+                val = val + ((*(matrix1.data + matrix1.size_x * i + k)) *
+                            (*(matrix2.data + k * matrix2.size_x + j)));
+                //data1[i][k] * data2[k][j];
             }
 
             data3[i][j] = val;
 
-            printf("c[%d,%d] = %3d\n", i + 1, j + 1, data3[i][j]);
+            printf("c[%d,%d] = %3d ", i, j, data3[i][j]);
         }
     }
+    matrix3.data = *data3;
 }
