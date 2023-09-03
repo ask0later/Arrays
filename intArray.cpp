307250119de20a3dc9f97ae977be5b24dbdad422
ask0later <139331444+ask0later@users.noreply.github.com>
2023-09-03 16:19:22 +0300

    Update intArray.cpp

--- intArray.cpp
+++ intArray.cpp
@@ -4,74 +4,46 @@
 struct MATRIX
 {
     int* data;
-    const size_t size_y, size_x;
+    size_t size_y, size_x;
 };
 
-
-void PrintData1(MATRIX matrix1);
-void PrintData2(MATRIX matrix2);
+void EnterMatrix(int* ptr, size_t* selectY, size_t* selectX);
+void PrintData(size_t size_y, size_t size_x, int* array);
 void ResultProduct(MATRIX matrix1, MATRIX matrix2);
 int SelectionProduct(MATRIX matrix1, MATRIX matrix2);
 
 
 int main()
 {
-    MATRIX matrix1 = {.data = 0, .size_y = 3, .size_x = 2};
-    MATRIX matrix2 = {.data = 0, .size_y = 2, .size_x = 3};
+    MATRIX matrix1 = {.data = 0, .size_y = 0, .size_x = 0};
+    MATRIX matrix2 = {.data = 0, .size_y = 0, .size_x = 0};
+    EnterMatrix(matrix1.data, &matrix1.size_y, &matrix1.size_x);
+    EnterMatrix(matrix2.data, &matrix2.size_y, &matrix2.size_x);
 
-    /*const matrix1.size_y = 3;
-    const matrix1.size_x = 2;
+    PrintData(matrix1.size_y, matrix1.size_x, matrix1.data);
+    PrintData(matrix2.size_y, matrix2.size_x, matrix2.data);
 
-    const matrix2.size_y = 2;
-    const matrix2.size_x = 3;*/
-
-    int data1[matrix1.size_y][matrix1.size_x] = {{2,  3},
-                                                 {3, -1},
-                                                 {-3, 2}};
-
-    int data2[matrix2.size_y][matrix2.size_x] = {{2, -3, -1},
-                                                 {3,  1,  1}};
-
-    matrix1.data = *data1;
-    matrix2.data = *data2;
-
-    PrintData1(matrix1);
-    PrintData2(matrix2);
-    printf("\n");
 
     ResultProduct(matrix1, matrix2);
+
     // const int * [5][5]
 }
-void PrintData1(MATRIX matrix1)
+void PrintData(size_t size_y, size_t size_x, int* array)
 {
-    for (size_t i = 0; i < matrix1.size_y; i++)
+    for (size_t i = 0; i < size_y; i++)
     {
         printf("\n");
-        for (size_t j = 0; j < matrix1.size_x; j++)
+        for (size_t j = 0; j < size_x; j++)
         {
             printf("a[%d, %d] = ", i, j);
                                     // size_x
-            printf("%2d  ", *(matrix1.data + matrix1.size_x * i + j));
+            printf("%4d  ", *(array + size_x * i + j));
         }
     }
     printf("\n");
 }
 
-void PrintData2(MATRIX matrix2)
-{
-    for (size_t i = 0; i < matrix2.size_y; i++)
-    {
-        printf("\n");
-        for (size_t j = 0; j < matrix2.size_x; j++)
-        {
-            printf("b[%d, %d] = ", i, j);
-                                    // size_x
-            printf("%2d  ", *(matrix2.data + matrix2.size_x * i + j));
-        }
-    }
 
-    printf("\n");
-}
 
 int SelectionProduct(MATRIX matrix1, MATRIX matrix2)
 {
@@ -94,7 +66,6 @@ int SelectionProduct(MATRIX matrix1, MATRIX matrix2)
 
 void ResultProduct(MATRIX matrix1, MATRIX matrix2)
 {
-
     MATRIX matrix3 = {.data = 0, .size_y = matrix1.size_y, .size_x = matrix2.size_x};
     int data3[matrix3.size_y][matrix3.size_x] = {0};
     size_t n = matrix1.size_x;
@@ -109,13 +80,22 @@ void ResultProduct(MATRIX matrix1, MATRIX matrix2)
             {
                 val = val + ((*(matrix1.data + matrix1.size_x * i + k)) *
                             (*(matrix2.data + k * matrix2.size_x + j)));
-                //data1[i][k] * data2[k][j];
             }
-
             data3[i][j] = val;
-
-            printf("c[%d,%d] = %3d ", i, j, data3[i][j]);
         }
     }
     matrix3.data = *data3;
+    PrintData(matrix3.size_y, matrix3.size_x, matrix3.data);
+}
+
+void EnterMatrix(int* ptr, size_t* selectY, size_t* selectX)
+{
+    printf("Enter rows and cols of matrix by rotation:\n");
+    scanf("%d %d", selectY, selectX);
+    printf("enter ech number of matrix:\n");
+    ptr = (int*)calloc((*selectY) * (*selectX), sizeof(int));
+    for (size_t i = 0; i < (*selectY) * (*selectX); i++)
+    {
+        scanf("%d",ptr + i);
+    }
 }
